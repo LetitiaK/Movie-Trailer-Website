86b459a6a9219099172009c1d34dc524de6a2df8
LetitiaK <letitia.kernschmidt@gmail.com>
2017-10-13 19:23:47 +0200

    Update media.py

--- media.py
+++ media.py
@@ -1,10 +1,10 @@
 class Movie():
-  """ This class is used to define movies."""
+    """ This class is used to define movies."""
 
-  def __init__(self, title, poster_image_url, trailer_youtube_url):
-      """Inits the class Movie with the variables title, poster_image_url,
-      and trailer_youtube_url
-      """
-      self.title = title
-      self.poster_image_url = poster_image_url
-      self.trailer_youtube_url = trailer_youtube_url
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        """Inits the class Movie with the variables title, poster_image_url,
+        and trailer_youtube_url
+        """
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
