72c8c1a05c3bbc3089c4ad48fd08956100d0a9e8
LetitiaK <letitia.kernschmidt@gmail.com>
2017-10-13 19:22:45 +0200

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,15 +6,15 @@ pretty_woman = media.Movie("Pretty Woman",
                            "https://www.youtube.com/watch?v=Wzii8IuL8lk")
 
 love_actually = media.Movie("Love actually",
-                            "https://upload.wikimedia.org/wikipedia/en/e/eb/Love_Actually_movie.jpg",  #NOQA
+                            "https://upload.wikimedia.org/wikipedia/en/e/eb/Love_Actually_movie.jpg",  # NOQA
                             "https://www.youtube.com/watch?v=fOS-HMiVejo")
 
 amelie = media.Movie("Amelie",
-                     "https://upload.wikimedia.org/wikipedia/en/5/53/Amelie_poster.jpg",  #NOQA
+                     "https://upload.wikimedia.org/wikipedia/en/5/53/Amelie_poster.jpg",  # NOQA
                      "https://www.youtube.com/watch?v=LM0sZZ1xFDs")
 
 the_millers = media.Movie("We're the Millers",
-                          "https://upload.wikimedia.org/wikipedia/en/4/44/We%27re_the_Millers_poster.jpg",  #NOQA
+                          "https://upload.wikimedia.org/wikipedia/en/4/44/We%27re_the_Millers_poster.jpg",  # NOQA
                           "https://www.youtube.com/watch?v=0Vsy5KzsieQ")
 
 aristocats = media.Movie("The Aristocats",
@@ -24,7 +24,7 @@ aristocats = media.Movie("The Aristocats",
 
 hunger_games_part_1 = media.Movie("The Hunger Games: Mockingjay Part 1",
                                   "https://upload.wikimedia.org/wikipedia/en/6/63/MockingjayPart1Poster3.jpg",  # NOQA
-                                  "https://www.youtube.com/watch?v=3PkkHsuMrho")
+                                  "https://www.youtube.com/watch?v=3PkkHsuMrho")  # NOQA
 
 # Creates a list of all the movie instances
 movies = [pretty_woman, love_actually, amelie, the_millers,
